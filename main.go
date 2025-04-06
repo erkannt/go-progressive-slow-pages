97b1964fea787eb1f221109b0b9242fdf3ab70e0
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-04-06 18:35:18 +0100

    Remove superfluous argument

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return landing("World").Render(c.Request().Context(), c.Response().Writer)
+		return landing().Render(c.Request().Context(), c.Response().Writer)
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }

303ecc379264c30d0e2fed378ad4c063026aa69c
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-04-06 18:40:54 +0100

    A search results page that populates the the query from a query param

--- main.go
+++ main.go
@@ -9,5 +9,9 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return landing().Render(c.Request().Context(), c.Response().Writer)
 	})
+	e.GET("/search", func(c echo.Context) error {
+		query := c.QueryParam("q")
+		return search(query).Render(c.Request().Context(), c.Response().Writer)
+	})
 	e.Logger.Fatal(e.Start(":8080"))
 }

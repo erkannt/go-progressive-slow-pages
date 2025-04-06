4ae9e905922a1c07595452975d830a553e4942fe
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-04-06 18:53:11 +0100

    Reduce duplication by extracting templ components

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return landing().Render(c.Request().Context(), c.Response().Writer)
+		return page(landing()).Render(c.Request().Context(), c.Response().Writer)
 	})
 	e.GET("/search", func(c echo.Context) error {
 		query := c.QueryParam("q")
-		return search(query).Render(c.Request().Context(), c.Response().Writer)
+		return page(search(query)).Render(c.Request().Context(), c.Response().Writer)
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
